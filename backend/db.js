0f8856a73b393fe2eac086ae065dfd0b98dcbac2
Eddie Kim <sungmin.kim082@gmail.com>
2020-11-08 01:49:09 -0500

    added port number

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,8 @@ const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQL_PORT
+  database: process.env.MYSQL_PORT,
+  port: process.env.MYSQL_PORT
 })
 
 exports.pool = pool
