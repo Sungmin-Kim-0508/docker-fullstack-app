f9aabd42e334806e90b8fd14b9c3be6928e1adca
Eddie Kim <sungmin.kim082@gmail.com>
2020-11-08 16:19:28 -0500

    changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -42,12 +42,9 @@ function App() {
           <h1>환영합니다!</h1>
         </div>
         <div className="container">
-          {lists && lists.map((list, index) => {
-            console.log(list)
-            return (
-              <li key={index}>Hello</li>
-            )
-          })}
+          {lists && lists.map((list, index) => (
+            <li key={index}>{list.value}</li>
+          ))}
           <form className="example" onSubmit={submitHandler}>
             <input
               type="text"
